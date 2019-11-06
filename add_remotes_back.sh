6dd09f70620584da27ea30cfb3bbadf5d43145c3
Richard Skumat <gitlab@richardskumat.com>
2019-11-06 09:01:25 +0000

    updat remotes[skip ci]

--- add_remotes_back.sh
+++ add_remotes_back.sh
@@ -1,3 +1,3 @@
 git remote add github git@github:richardskumat/ansible-role-sshd_config
 git remote add gitlab git@gitlab:richardskumat/ansible-role-sshd_config
-git remote add origin git@wsgitlab:qwe/ansible-role-sshd_config
+git remote add origin git@gitea:qwe/ansible-role-sshd_config
